c585e56cc0db7e2b4bcedb91a7bb3a47a4350e40
Jairo Abreu <Jairo.Abreu.3133@gmail.com>
2020-01-11 00:26:08 -0400

    Add files via upload

---
+++ .config/polybar/scripts/ping2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+HOST=1.1.1.1
+
+if ! ping=$(ping -n -c 1 -W 1 $HOST 2> /dev/null); then
+    echo "%{F#BF616A}%{F-} ?"
+else
+    rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
+
+    if [ "$rtt" -lt 100 ]; then
+        #icon="%{F#A3BE8C}%{F-}"
+        icon="%{F#A3BE8C}%{F-}"
+    elif [ "$rtt" -lt 150 ]; then
+        icon="%{F#D08770}%{F-}"
+    else
+        icon="%{F#EBCB8B}%{F-}"
+    fi
+
+    echo "$icon $rtt ms "
+fi
