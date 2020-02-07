4ae56bcaef346cd08c21c6797a85181e748ef54e
Jairo Abreu <jairo.abreu.3133@gmail.com>
2020-02-07 14:48:31 +0000

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/wallpaper.sh'

--- polybar/.config/polybar/scripts/wallpaper.sh
+++ polybar/.config/polybar/scripts/wallpaper.sh
@@ -12,7 +12,7 @@
 dir="${XDG_PICTURES_DIR:-${HOME}/Pictures/wallpapers/linux}"
 unsplash_dir="${XDG_PICTURES_DIR:-${HOME}/Pictures}/unsplash"
 default="$dir/wallpapers/default.jpg"
-ultima="/home/jairo/Imagens/wallpapers/unsplash/unsplash-1837391.jpg"
+ultima="/home/jairo/Imagens/wallpapers/unsplash/unsplash-457386.jpg"
 modo="--bg-fill"
 indice=0
 i=0
