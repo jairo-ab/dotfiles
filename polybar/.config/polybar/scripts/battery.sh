f0746e32cdf807c0c7039abd0da646a04d6c914e
Jairo Abreu <jairo.abreu.3133@gmail.com>
2020-03-09 23:55:07 +0000

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/battery.sh'

--- polybar/.config/polybar/scripts/battery.sh
+++ polybar/.config/polybar/scripts/battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 batticon=(    )
-grep -q 1 /sys/class/power_supply/ADP/online && batt="%{F#FFFFFF}%{F-}"
-for ((i=0; i<=$(($(ls -d /sys/class/power_supply/BAT0 | wc -l) - 1)); i++)); do
+grep -q 1 /sys/class/power_supply/ADP0/online && batt="%{F#FFFFFF}%{F-}"
+for ((i=0; i<=$(($(ls -d /sys/class/power_supply/BAT* | wc -l) - 1)); i++)); do
 	source /sys/class/power_supply/BAT$i/uevent 2> /dev/null
 	for j in {0..9}; do
 		[ $POWER_SUPPLY_CAPACITY -lt $((j * 10)) ] && batt+="%{F#8$((10 - j))$((j * 10))$((j * 10))}${batticon[((j + 1))/ 2]}%{F-} $POWER_SUPPLY_CAPACITY% " && break
