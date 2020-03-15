7397e632593bf571dbb41a6fc81d8a659bca171d
Jairo Abreu <jairo.abreu.3133@gmail.com>
2020-03-15 15:59:54 +0000

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/ajustemonitor.sh'

--- polybar/.config/polybar/scripts/ajustemonitor.sh
+++ polybar/.config/polybar/scripts/ajustemonitor.sh
@@ -20,7 +20,7 @@ res=$(printf "%s\n" "${selecao[@]}" | rofi $tema -dmenu -p "Rofi Config" -bw 0 -
 
 if [ "$res" ]; then
 	case $res in
-		'Conectar HDMI') exec $app --output eDP1 --mode 1366x768 --output HDMI1 --mode 1920x1080 --right-of HDMI1 & exec i3-msg restart ;;
+		'Conectar HDMI') exec $app --output eDP1 --mode 1366x768 --primary --output HDMI1 --mode 1920x1080 --right-of HDMI1 & exec i3-msg restart ;;
 		'Desconectar HDMI') exec $app --output HDMI1 --off & exec i3-msg restart ;;
 		'Espelhar HDMI') exec $app --output HDMI1 --output eDP1 --mode 1366x768 --same-as HDMI1 & exec i3-msg restart ;;
 		'Conectar VGA') exec $app --output DP1 --mode 1024x768 --primary --output eDP1 --mode 1366x768 --right-of DP1 & exec i3-msg restart ;;
