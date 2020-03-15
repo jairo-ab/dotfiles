9faa8e2cfec0cd6be2a7df177d3c401f225b0203
Jairo Abreu <jairo.abreu.3133@gmail.com>
2020-03-15 15:51:04 +0000

    Autocommit por GitHub.sh: alterações: add 'polybar/.config/polybar/scripts/ajustemonitor.sh'

--- polybar/.config/polybar/scripts/ajustemonitor.sh
+++ polybar/.config/polybar/scripts/ajustemonitor.sh
@@ -21,10 +21,10 @@ res=$(printf "%s\n" "${selecao[@]}" | rofi $tema -dmenu -p "Rofi Config" -bw 0 -
 if [ "$res" ]; then
 	case $res in
 		'Conectar HDMI') exec $app --output HDMI1 --mode 1920x1080 --primary --output eDP1 --mode 1366x768 --right-of HDMI1 & exec i3-msg restart ;;
-		'Desconectar HDMI') exec $app --output HDMI1 --off && exec i3-msg restart ;;
-		'Espelhar HDMI') exec $app --output eDP1 --mode 1366x768 --output HDMI1 --same-as eDP1 && exec i3-msg restart ;;
-		'Conectar VGA') exec $app --output DP1 --mode 1024x768 --primary --output eDP1 --mode 1366x768 --right-of DP1 && exec i3-msg restart ;;
-		'Desconectar VGA') exec $app --output DP1 --off && exec i3-msg restart ;;
-		'Espelhar VGA') exec $app --output eDP1 --output DP1 --same-as eDP1 && exec i3-msg restart ;; 
+		'Desconectar HDMI') exec $app --output HDMI1 --off & exec i3-msg restart ;;
+		'Espelhar HDMI') exec $app --output eDP1 --mode 1366x768 --output HDMI1 --same-as eDP1 & exec i3-msg restart ;;
+		'Conectar VGA') exec $app --output DP1 --mode 1024x768 --primary --output eDP1 --mode 1366x768 --right-of DP1 & exec i3-msg restart ;;
+		'Desconectar VGA') exec $app --output DP1 --off & exec i3-msg restart ;;
+		'Espelhar VGA') exec $app --output eDP1 --output DP1 --same-as eDP1 & exec i3-msg restart ;; 
 	esac
 fi
