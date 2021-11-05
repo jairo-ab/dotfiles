cbf5b5c05a37f9ab1bbdd37bbc06565c5e2f0de8
jairo-ab <jairo.abreu.3133@gmail.com>
2021-11-05 11:06:40 -0400

    Add path of file xfce4

---
+++ xfce4/.config/xfce4/scripts/menu-session.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+session="xfce4-session-logout"
+icon="/home/jairo/.icons/dracula-icons-main/22/actions/twitter-profile.svg"
+
+echo "<img>${icon}</img>"
+echo "<txt>Sessão</txt>"
+echo "<click>bash -c ${session}</click>"
