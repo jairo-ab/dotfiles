bcb7581c522bd893251c558826c9f340ab31249c
jairo-ab <jairo.abreu.3133@gmail.com>
2021-11-16 10:24:27 -0400

    Edit filee panel-xfce.sh

--- home/bin/panel-xfce.sh
+++ home/bin/panel-xfce.sh
@@ -2,11 +2,11 @@
 
 package='xfce4-panel-profiles'
 icon='/home/jairo/.local/share/icons/elementary/monitor.svg'
-if [ 'xrandr | awk "/HDMI2 / {print $2 }"' = 'connected' ]; then
+command=$(xrandr | awk '/HDMI2 / { print $2 }')
+if [ $command == 'connected' ]; then
   $package load /home/jairo/.local/share/xfce4-panel-profiles/work-panel2.tar.bz2
   notify-send -i $icon 'Painel duplo aplicado'
 else 
   $package load /home/jairo/.local/share/xfce4-panel-profiles/Pessoal-Panel.tar.bz2
   notify-send -i $icon 'Painel primário aplicado'
 fi
-
