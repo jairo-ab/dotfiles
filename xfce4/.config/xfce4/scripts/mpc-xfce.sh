8fc75fe4624a0fa46b58e93d23e15e18deddb70f
jairo-ab <jairo.abreu.3133@gmail.com>
2022-02-15 00:44:49 -0400

    Autocommit por GitHub: alterações: add 'xfce4/.config/xfce4/scripts/mpc-xfce.sh' add 'home/.local/share/icons/elementary/player_start.svg'

--- xfce4/.config/xfce4/scripts/mpc-xfce.sh
+++ xfce4/.config/xfce4/scripts/mpc-xfce.sh
@@ -2,9 +2,14 @@
 
 command=$(mpc current)
 status=$(mpc | grep playing | sed 's/|.*|/|/' | awk '{ print $3 }')
+#play=$(mpc toggle)
+
+icon="$HOME/.local/share/icons/elementary/player_start.svg"
 
 if [ "$command" ]; then
-  echo "<txt>  ${command} - ${status}</txt>"
+  echo "<click>bash -c $HOME/bin/mpc-toggle.sh</click>"
+  echo "<img>${icon}</img>"
+  echo "<txt> ${command} - ${status}</txt>"
 else
   echo "<txt></txt>"
 fi
